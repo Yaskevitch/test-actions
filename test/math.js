c49599be308cb73c2506120fc29ef141e9bb5bb8
Yaskevitch <v.yaskevitch@gmail.com>
2020-11-04 16:33:37 +0200

    add function sum, test for sum

---
+++ test/math.js
@@ -0,0 +1,18 @@
+const sum = require('../math');
+const assert = require('assert');
+
+describe('Math', function () {
+    describe('sum', function () {
+        it('should sum 1 and 2', function () {
+            let actual = sum(1, 2);
+
+            assert.strictEqual(actual, 3);
+        });
+        it('should sum "12" and 7', function () {
+            let actual = sum('12', 7);
+
+            assert.strictEqual(actual, 19);
+        });
+    });
+    
+});
