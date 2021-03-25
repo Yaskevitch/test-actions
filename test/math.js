bc1b9d0b62640c7a89ad60c289b0c710062ef854
Yaskevitch <v.yaskevitch@gmail.com>
2021-03-25 18:31:13 +0200

    Update math.js

--- test/math.js
+++ test/math.js
@@ -3,11 +3,18 @@ const assert = require('assert');
 
 describe('Math', function () {
     describe('sum', function () {
+        it('should sum 3 and 0', function () {
+            let actual = sum(3, 0);
+
+            assert.strictEqual(actual, 3);
+        });
+        
         it('should sum 1 and 2', function () {
             let actual = sum(1, 2);
 
             assert.strictEqual(actual, 3);
         });
+        
         it('should sum "12" and 7', function () {
             let actual = sum('12', 7);
 
